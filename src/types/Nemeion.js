4e055e3f65a2a89efa8c5dc3f96724c9ee4df010
Nathan Harris <nathan@mordil.info>
2023-12-29 13:23:13 -0600

    Use default data values in Nemeion constructor

--- src/types/Nemeion.js
+++ src/types/Nemeion.js
@@ -1,12 +1,14 @@
+import DATA from '@/data.yaml'
+
 export default class Nemeion {
     constructor(initialValues = {}) {
         this.gender = initialValues.gender || null
-        this.fur = initialValues.fur || null
-        this.coat = initialValues.coat || null
-        this.build = initialValues.build || null
-        this.traits = initialValues.traits || []
-        this.markings = initialValues.markings || []
-        this.mutations = initialValues.mutations || []
+        this.fur = initialValues.fur || DATA.furs.default
+        this.coat = initialValues.coat || DATA.coats.default
+        this.build = initialValues.build || DATA.builds.default
+        this.traits = initialValues.traits || DATA.traits.default
+        this.markings = initialValues.markings || DATA.markings.default
+        this.mutations = initialValues.mutations || DATA.mutations.default
     }
 
     get hasTraits() { return this.traits.length > 0 }
