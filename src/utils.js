0b1fd7af5a44b38b8890318210aa134924d27920
Nathan Harris <nathan@mordil.info>
2025-01-16 22:24:25 -0600

    Add utility for sorting objects by their keys

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
 export function rollForThreshold(threshold, tolerance = 0.01) {
     return Math.random() <= threshold + tolerance
 }
+
+export function sortData(data) {
+    return Object
+        .keys(data)
+        .sort()
+        .reduce((obj, key) => {
+            obj[key] = data[key]
+            return obj
+        }, {})
+}
